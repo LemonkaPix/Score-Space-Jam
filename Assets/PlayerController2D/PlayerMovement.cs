7138870ca5f06f678a8bc24f11db135b3f394525
LemonkaPix <maciejwrobel029@gmail.com>
2023-01-20 22:43:59 +0100

    evo-data

---
+++ Assets/PlayerController2D/PlayerMovement.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 5f;
+    [SerializeField] Rigidbody2D rb;
+    Vector2 movement;
+    
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+}
