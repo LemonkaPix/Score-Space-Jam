a9da1bde543ace7c0345593716dd589a59be1f65
LemonkaPix <maciejwrobel029@gmail.com>
2023-01-23 15:49:28 +0100

    update

--- Assets/LeaderBoard.cs
+++ Assets/LeaderBoard.cs
@@ -1,10 +1,7 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using LootLocker.Requests;
-using UnityEditor.Timeline.Actions;
 using TMPro;
-using Unity.VisualScripting;
 
 public class LeaderBoard : MonoBehaviour
 {
